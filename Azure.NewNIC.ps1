e648fcf3845f2bc91038e58daa1031b78c88227d
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2022-06-04 19:34:14 +0800

    Delete Azure.NewNIC.ps1

--- Azure.NewNIC.ps1
+++
@@ -1,8 +0,0 @@
-$NetworkInterfaceName = "NetworkInterface1"
-
-$ResourceGroupName = "ResourceGroup1"
-$Location= "centralus"
-
-New-AzNetworkInterface -Name $NetworkInterfaceName -ResourceGroupName $ResourceGroupName -Location $Location -SubnetId "/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourceGroups/ResourceGroup1/providers/Microsoft.Network/virtualNetworks/VirtualNetwork1/subnets/Subnet1" -IpConfigurationName "IPConfiguration1" -DnsServer "8.8.8.8", "8.8.4.4"
-
-

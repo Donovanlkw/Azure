f86baee43cc2cdf6d79deedb7eb363207463e212
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2022-06-04 19:31:15 +0800

    Update AAD.user.ps1

--- AAD.user.ps1
+++ AAD.user.ps1
@@ -1,17 +1,3 @@
-#---- Add O365 user in AAD for Sync -----# 
-$userlist = "Bob", "Steve", "Mary", "Peter", "John", "Annie" 
-$Password =  "Password1" 
-$Encryptedpassword=$Password | ConvertTo-SecureString -Force -AsPlainText 
-
-$userlist |foreach-object { 
-$UserPrincipalName = "$_@$orgName.onMicrosoft.com" 
-New-ADUser -Name "$_" -GivenName "$_" -Surname "" -SamAccountName "$_" -UserPrincipalName "$_@$Domainname" -Path "OU=O365,$ou" -AccountPassword($Encryptedpassword) -Enabled $true 
-} 
-#New-AzureADUser -DisplayName "New User" -PasswordProfile $PasswordProfile  -UserPrincipalName "newuser2@guestxthoutlook.onmicrosoft.com" -AccountEnabled $true -MailNickName "newuser2"
-
-
-#---- Create a Azure Dynamic Group -----#
-New-AzureADMSGroup -DisplayName "Dynamic Group 01" -Description "Dynamic group created from PS" -MailEnabled $False -MailNickName "group" -SecurityEnabled $True -GroupTypes "DynamicMembership" -MembershipRule "(user.department -contains ""Marketing"")" -MembershipRuleProcessingState "On"
 
 
 #---- Prepare a non-routable domain for directory synchronization -----#
@@ -21,7 +7,7 @@ Get-ADForest | Format-List UPNSuffixes
 Get-ADForest | Set-ADForest -UPNSuffixes @{add="$orgName.onMicrosoft.com"}
 
 
-#---- Add O365 user in AD for Sync -----#
+#---- Add O365 user in AAD for Sync -----#
 $userlist = "Bob", "Steve", "Mary", "Peter", "John", "Annie"
 $Password =  "Password1"
 $Encryptedpassword=$Password | ConvertTo-SecureString -Force -AsPlainText
@@ -29,12 +15,16 @@ $userlist |foreach-object {
 $UserPrincipalName = "$_@$orgName.onMicrosoft.com"
 New-ADUser -Name "$_" -GivenName "$_" -Surname "" -SamAccountName "$_" -UserPrincipalName "$_@$Domainname" -Path "OU=O365,$ou" -AccountPassword($Encryptedpassword) -Enabled $true
 }
+#New-AzureADUser -DisplayName "New User" -PasswordProfile $PasswordProfile  -UserPrincipalName "newuser2@guestxthoutlook.onmicrosoft.com" -AccountEnabled $true -MailNickName "newuser2"
+
+
+#---- Create a Azure Dynamic Group -----#
+New-AzureADMSGroup -DisplayName "Dynamic Group 01" -Description "Dynamic group created from PS" -MailEnabled $False -MailNickName "group" -SecurityEnabled $True -GroupTypes "DynamicMembership" -MembershipRule "(user.department -contains ""Marketing"")" -MembershipRuleProcessingState "On"
 
 
 #---- Change UPN suffix for AAD O365 -----#
 $O365Users= Get-ADUser -Filter * -SearchBase "OU=O365,$ou"
 $O365Users | foreach {$newUpn = $_.UserPrincipalName.Replace("@$Domainname","@$orgName.onMicrosoft.com"); $_ | Set-ADUser -UserPrincipalName $newUpn}
-
 
 
 #---- Assing O365 license @ AAD -----#

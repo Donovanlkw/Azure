822b79c4699ba9f8eee439be0cf1fc3e15ab254e
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2022-06-04 19:29:35 +0800

    Update and rename Azure.ADD to AAD.user.ps1

--- AAD.user.ps1
+++ AAD.user.ps1
@@ -1,13 +1,17 @@
-#---- Add AAD user -----#
-$userlist = "Bob", "Steve", "Mary", "Peter", "John", "Annie"
-$PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
-$PasswordProfile.Password = "Password1"
-New-AzureADUser -DisplayName "New User" -PasswordProfile $PasswordProfile  -UserPrincipalName "newuser2@guestxthoutlook.onmicrosoft.com" -AccountEnabled $true -MailNickName "newuser2"
+#---- Add O365 user in AAD for Sync -----# 
+$userlist = "Bob", "Steve", "Mary", "Peter", "John", "Annie" 
+$Password =  "Password1" 
+$Encryptedpassword=$Password | ConvertTo-SecureString -Force -AsPlainText 
+
+$userlist |foreach-object { 
+$UserPrincipalName = "$_@$orgName.onMicrosoft.com" 
+New-ADUser -Name "$_" -GivenName "$_" -Surname "" -SamAccountName "$_" -UserPrincipalName "$_@$Domainname" -Path "OU=O365,$ou" -AccountPassword($Encryptedpassword) -Enabled $true 
+} 
+#New-AzureADUser -DisplayName "New User" -PasswordProfile $PasswordProfile  -UserPrincipalName "newuser2@guestxthoutlook.onmicrosoft.com" -AccountEnabled $true -MailNickName "newuser2"
+
 
 #---- Create a Azure Dynamic Group -----#
 New-AzureADMSGroup -DisplayName "Dynamic Group 01" -Description "Dynamic group created from PS" -MailEnabled $False -MailNickName "group" -SecurityEnabled $True -GroupTypes "DynamicMembership" -MembershipRule "(user.department -contains ""Marketing"")" -MembershipRuleProcessingState "On"
-
-
 
 
 #---- Prepare a non-routable domain for directory synchronization -----#
